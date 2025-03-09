29fd6f70c534ab2e9d529d61ca2b75363dd75380
Amina-Delali <aimosta.official@gmail.com>
2025-03-09 09:20:58 +0100

    python version 3.12

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py makemigration --noinput
-python3.9 manage.py migrate --noinput
+python3.12 manage.py collectstatic --noinput
+python3.12 manage.py makemigration --noinput
+python3.12 manage.py migrate --noinput
